3058e97fc81abfc2f1faaf02850d8bb4ec6fffd3
김대열 <ajduf7180@gmail.com>
2023-08-28 11:07:15 +0900

    [Add] 게임플레이 담당클래스 생성 (-대열) 메인 함수에서 게임을 시작하는 클래스 생성

---
+++ ZzabDenRing/Game.cs
@@ -0,0 +1,12 @@
+namespace ZzabDenRing;
+
+public class Game
+{
+    // todo screen display
+    // todo di
+    // todo database or file system
+
+    public void Start()
+    {
+    }
+}
--- ZzabDenRing/Program.cs
+++ ZzabDenRing/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ZzabDenRing;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var game = new Game();
+        game.Start();
+    }
+}
