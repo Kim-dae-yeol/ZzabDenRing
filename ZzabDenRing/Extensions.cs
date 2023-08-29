583252c0b53e17eeae788575ffd78ca2460078b7
김대열 <ajduf7180@gmail.com>
2023-08-29 14:26:07 +0900

    [Update] 확장함수.슬롯 타입 스트링 추가 (-대열)
    - 슬롯에 따른 한글 문자열을 가져오는 함수 추가

--- ZzabDenRing/Extensions.cs
+++ ZzabDenRing/Extensions.cs
@@ -1,3 +1,5 @@
+using ZzabDenRing.Model;
+
 namespace ZzabDenRing;
 
 public static class Extensions
@@ -9,4 +11,21 @@ public static class Extensions
     public static int UnicodeCount(this string s) => s.Count(c => c.IsHanGul());
 
     public static int LengthToDisplay(this string s) => s.Length + s.UnicodeCount();
+    
+    public static string String(this EquipmentSlot s)
+    {
+        return s switch
+        {
+            EquipmentSlot.Helm => "투구",
+            EquipmentSlot.Necklace => "목걸이",
+            EquipmentSlot.Weapon => "무기",
+            EquipmentSlot.Armor => "갑옷",
+            EquipmentSlot.SubWeapon => "보조무기",
+            EquipmentSlot.Pants => "바지",
+            EquipmentSlot.Ring1 => "반지1",
+            EquipmentSlot.Ring2 => "반지2",
+            EquipmentSlot.Boots => "신발",
+            _ => ""
+        };
+    }
 }
