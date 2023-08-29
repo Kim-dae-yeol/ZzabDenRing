3c7d113b5f65ada178756580c9f61e11fc3ad741
DoroNyong <pride5623@naver.com>
2023-08-29 11:28:33 +0900

    [Add] 던전입구 클래스파일 추가 (-현교)

---
+++ ZzabDenRing/Screens/Dungeon/DungeonEntranceScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzabDenRing.Screens.Dungeon
+{
+    internal class DungeonEntranceScreen : BaseScreen
+    {
+        protected override void DrawContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool ManageInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
