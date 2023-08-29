006a1bd1f60835e9429a1d898a81f011582477f3
JangbeomChoi <jangbeom0325@gmail.com>
2023-08-29 14:01:55 +0900

    [Add] 상태창스크린 파일 추가(-장범)
    -상태창 Status창 파일 추가했습니다.

---
+++ ZzabDenRing/Screens/Status/StatusScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzabDenRing.Screens.Status
+{
+    internal class StatusScreen : BaseScreen
+    {
+        protected override void DrawContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool ManageInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
