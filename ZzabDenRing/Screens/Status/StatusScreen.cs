5f4127ccc6471d9922e84be6e4b2948ea49f15f7
JangbeomChoi <jangbeom0325@gmail.com>
2023-08-29 16:10:05 +0900

    [Update]상태창 생성자 생성(-장범)
    -상태창에 대열님이 말씀해주신 생성자 추가했습니다.

--- ZzabDenRing/Screens/Status/StatusScreen.cs
+++ ZzabDenRing/Screens/Status/StatusScreen.cs
@@ -6,17 +6,25 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using ZzabDenRing.Model;
+using System.Reflection.Emit;
+using System.Xml.Linq;
 
 namespace ZzabDenRing.Screens.Status
 {
+    
     internal class StatusScreen : BaseScreen
     {
         private Stack<ScreenType> _backStack = new(10);
         internal IReadOnlyCollection<ScreenType> BackStack => _backStack;
 
         public Character player;
+        internal StatusScreen()
+        {
+            player = new Character("이름","직업",100, 100, 10, 01, 5, 1500, 15, new List<Item>(),new Equipment());
+        }
         protected override void DrawContent()
         {
+
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("상태 보기");
@@ -24,17 +32,20 @@ namespace ZzabDenRing.Screens.Status
 
             Console.WriteLine("캐릭터의 정보가 표시됩니다.");
             Console.WriteLine();
-
-            Console.WriteLine($"Lv.{player.Level}");
-            Console.WriteLine($"{player.Name}");
-            Console.WriteLine($"{player.Job}");         
-            Console.WriteLine($"공격력 :{player.Atk}");
-            Console.WriteLine($"방어력 :{player.Def}");
+            Console.WriteLine("########################################");
+            Console.WriteLine();
+            Console.WriteLine($"Lv.{player.Level} "); 
+            Console.WriteLine($"{player.Name}: ");
+            Console.WriteLine($"{player.Job}: ");         
+            Console.WriteLine($"공격력 : {player.Atk}");
+            Console.WriteLine($"방어력 : {player.Def}");
             Console.WriteLine($"체력 : {player.Hp}");
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine($"Gold : {player.Gold}");
             Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("########################################");
 
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
@@ -49,37 +60,19 @@ namespace ZzabDenRing.Screens.Status
             var key = ReadKey();
             var command = key.Key switch
             {
+                ConsoleKey.UpArrow => Command.MoveTop,
+                ConsoleKey.RightArrow => Command.MoveRight,
+                ConsoleKey.DownArrow => Command.MoveBottom,
+                ConsoleKey.LeftArrow => Command.MoveLeft,
+                ConsoleKey.Enter => Command.Interaction,
                 ConsoleKey.X => Command.Exit,
                 _ => Command.Nothing
             };
-            int selectedAction = CheckValidInput(0, 0);
+            
 
-            switch (selectedAction)
-            {
-                case 0:
-                    _backStack.Push(ScreenType.Main);// 상태보기 화면으로 이동
-                    break;
-                
-            }
-
-            return true;
+            return command != Command.Exit;
         }
-        static int CheckValidInput(int min, int max) //입력된 숫자가 valid한지를 체크
-        {
-            while (true)
-            {
-                string input = Console.ReadLine();
-
-                bool parseSuccess = int.TryParse(input, out var ret);
-                if (parseSuccess)
-                {
-                    if (ret >= min && ret <= max)
-                        return ret;
-                }
-
-                Console.WriteLine("잘못된 입력입니다.");
-            }
-        }
+        
 
     }
 }
