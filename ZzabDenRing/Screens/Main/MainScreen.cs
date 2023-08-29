b48a288e45e213370d9ca3bb32b5998205313b3d
JangbeomChoi <jangbeom0325@gmail.com>
2023-08-29 12:53:31 +0900

    [Fix] 한국어 인코딩 문제 수정(-장범)
    -using System.Text; 추가를 안해서 한글 출력이 안되는 오류 수정

--- ZzabDenRing/Screens/Main/MainScreen.cs
+++ ZzabDenRing/Screens/Main/MainScreen.cs
@@ -1,5 +1,10 @@
 using ZzabDenRing.Screens.Home;
 using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ZzabDenRing.Screens.Main;
 
