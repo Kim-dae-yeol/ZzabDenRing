42943a144df690edb51eed3d98b8dde8e1a1e2b1
김대열 <ajduf7180@gmail.com>
2023-08-30 19:45:23 +0900

    [Fix] 던전 보상 화면 수정(-대열)
    던전보상화면에서 아이템이 정상 출력되도록 수정

--- ZzabDenRing/Screens/Dungeon/DungeonRewardScreen.cs
+++ ZzabDenRing/Screens/Dungeon/DungeonRewardScreen.cs
@@ -12,28 +12,28 @@ namespace ZzabDenRing.Screens.Dungeon
     public class DungeonRewardScreen : BaseScreen
     {
         public Character player;
-        public Reward reward;   
-        
+        public Reward reward;
+
         private Action _navToDungeonEntrance;
         private Action _navToMain;
+
         public DungeonRewardScreen(Action navToDungeonEntrance,
-        Action navToMain)
+            Action navToMain)
         {
             _navToMain = navToMain;
             _navToDungeonEntrance = navToDungeonEntrance;
             player = new Character("이름", "직업", 200, 100, 10, 1, 5, 1500, 15, new List<Item>(),
-                new Model.Equipment());      
-            Item[] items = new Item[0];
-            reward = new Reward(1500, items);
+                new Model.Equipment());
+            reward = new Reward(1500, Game.Items.Take(3).ToArray());
+        }
 
-
-    }
         protected override void DrawContent()
         {
             Console.Clear();
 
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\r\n  ____        _   _   _      _            _____                 _ _   \r\n |  _ \\      | | | | | |    | |          |  __ \\               | | |  \r\n | |_) | __ _| |_| |_| | ___| |  ______  | |__) |___  ___ _   _| | |_ \r\n |  _ < / _` | __| __| |/ _ \\ | |______| |  _  // _ \\/ __| | | | | __|\r\n | |_) | (_| | |_| |_| |  __/_|          | | \\ \\  __/\\__ \\ |_| | | |_ \r\n |____/ \\__,_|\\__|\\__|_|\\___(_)          |_|  \\_\\___||___/\\__,_|_|\\__|\r\n                                                                      \r\n                                                                      \r\n");
+            Console.WriteLine(
+                "\r\n  ____        _   _   _      _            _____                 _ _   \r\n |  _ \\      | | | | | |    | |          |  __ \\               | | |  \r\n | |_) | __ _| |_| |_| | ___| |  ______  | |__) |___  ___ _   _| | |_ \r\n |  _ < / _` | __| __| |/ _ \\ | |______| |  _  // _ \\/ __| | | | | __|\r\n | |_) | (_| | |_| |_| |  __/_|          | | \\ \\  __/\\__ \\ |_| | | |_ \r\n |____/ \\__,_|\\__|\\__|_|\\___(_)          |_|  \\_\\___||___/\\__,_|_|\\__|\r\n                                                                      \r\n                                                                      \r\n");
             Console.ResetColor();
 
             Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -42,7 +42,22 @@ namespace ZzabDenRing.Screens.Dungeon
             Console.WriteLine();
 
             Console.WriteLine($"남은 체력 : {player.Hp}");
-            Console.WriteLine($"획득 아이템 :{reward.Items} ");
+            Console.Write($"획득 아이템 : ");
+
+            foreach (var pair in reward.Items.Select((item, i) => new { item, i }))
+            {
+                var item = pair.item;
+                var i = pair.i;
+                if (i == reward.Items.Length - 1)
+                {
+                    Write(item.Name);
+                }
+                else
+                {
+                    Write(item.Name + ", ");
+                }
+            }
+            WriteLine();
             Console.WriteLine($"획득 골드 : {reward.Gold} G");
             Console.WriteLine();
 
@@ -65,7 +80,7 @@ namespace ZzabDenRing.Screens.Dungeon
                     break;
                 case ConsoleKey.D2:
                     _navToMain();
-                    break;              
+                    break;
             }
 
             return false;
