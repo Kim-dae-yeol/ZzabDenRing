22126b8fcde6bcf501be07907ddde70e0c897010
김대열 <ajduf7180@gmail.com>
2023-08-30 11:48:29 +0900

    [Update] 캐릭터 선택창(-대열) - 작업중 다른 브런치로 이동하기위해 잠시 저장

--- ZzabDenRing/Screens/Home/HomeViewModel.cs
+++ ZzabDenRing/Screens/Home/HomeViewModel.cs
@@ -1,36 +1,47 @@
+using ZzabDenRing.Model;
+
 namespace ZzabDenRing.Screens.Home;
 
 public class HomeViewModel
 {
-    private IAsyncEnumerable<HomeState> _state;
-    public IAsyncEnumerable<HomeState> State => _state;
+    public IAsyncEnumerable<HomeSplashState> AnimationState { get; private set; }
+    private HomeState _homeState;
+    public HomeState HomeState => _homeState;
+
 
     public HomeViewModel()
     {
-        _state = GetHomeState();
-        
+        // todo get characters from repo
+        var characters = new List<Character>();
+        characters.AddRange(new[] { Game.C1, Game.C2, Game.C3 });
+        AnimationState = GetHomeState();
+        _homeState = new HomeState(0, characters);
     }
 
-    private async IAsyncEnumerable<HomeState> GetHomeState()
+    private async IAsyncEnumerable<HomeSplashState> GetHomeState()
     {
         var lines = Constants.HomeAsciiArt.Split("\n");
         int centerX = lines.Max(s => s.Length) / 2;
         int centerY = lines.Length / 2;
-        var homeState = new HomeState(currentHomeString: "",
-            centerX: centerX,
-            centerY: centerY);
+        var homeState = new HomeSplashState(CurrentHomeString: "",
+            CenterX: centerX,
+            CenterY: centerY);
 
         for (var i = 1; i <= Constants.HomeAsciiArt.Length; i++)
         {
             //await for 2 sec
             await Task.Delay((int)Math.Ceiling(Constants.HomeAsciiArt.Length / 2_000f));
-            yield return homeState with { currentHomeString = Constants.HomeAsciiArt[..i] };
+            yield return homeState with { CurrentHomeString = Constants.HomeAsciiArt[..i] };
         }
     }
 }
 
-public record HomeState(
-    int centerX,
-    int centerY,
-    string currentHomeString = ""
+public record HomeSplashState(
+    int CenterX,
+    int CenterY,
+    string CurrentHomeString = ""
 );
+
+public record HomeState(
+    int CurX,
+    IReadOnlyCollection<Character> Characters);
